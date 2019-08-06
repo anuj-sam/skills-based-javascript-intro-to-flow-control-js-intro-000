2ca5cdb8a8e8c017b9cad03d993644b05136c97e
elegant-mouse-4160 <anuj.samuellakra@gmail.com>
2019-08-06 01:39:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,8 +25,8 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
- switch (age) {
-   case "":
+ switch (true) {
+   case age>=13 && age<=19:
      return "You are a teenager"
    default:
       return "You have an age"
